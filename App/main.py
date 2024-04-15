3c23e2f578c7f45b2ab6c0788e3ac981b3f69469
Davi Oliveira Francisco <136659092+dofrancisco@users.noreply.github.com>
2024-04-15 19:21:02 -0300

    atualizando app com possibilidade de carregar outros modelos

--- App/main.py
+++ App/main.py
@@ -2,7 +2,11 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
 import pandas as pd
 from joblib import load
 import sys
@@ -24,22 +28,64 @@ class PredictorApp(App):
         self.label = Label(text='Select an Excel file to predict', size_hint=(1, .1))
         layout.add_widget(self.label)
 
-        self.filechooser = FileChooserListView(filters=['*.xlsx'], size_hint=(1, .8))
-        layout.add_widget(self.filechooser)
+        open_excel_button = Button(text='Open Excel File', size_hint=(1, .1))
+        open_excel_button.bind(on_press=self.open_filechooser)
+        layout.add_widget(open_excel_button)
+        
+        open_model_button = Button(text='Load Model', size_hint=(1, .1))
+        open_model_button.bind(on_press=self.open_model_chooser)
+        layout.add_widget(open_model_button)
 
-        predict_button = Button(text='Predict', size_hint=(1, .1))
-        predict_button.bind(on_press=self.predict)
-        layout.add_widget(predict_button)
+        self.predict_button = Button(text='Predict', size_hint=(1, .1))
+        self.predict_button.bind(on_press=self.predict)
+        self.predict_button.disabled = True  # Disable the button until a file is selected
+        layout.add_widget(self.predict_button)
 
         return layout
 
+    def open_filechooser(self, instance, file_type='*.xlsx', title='Select an Excel file'):
+        user_path = os.path.expanduser('~')  # Gets the current user's home directory
+        content = FileChooserListView(filters=[file_type], path=user_path)
+        content.bind(on_submit=self.select_file)
+        
+        self.popup = Popup(title=title, content=content,
+                           size_hint=(0.9, 0.9))
+        self.popup.open()
+
+    def open_model_chooser(self, instance):
+        # Use a different title and file_type for the model file chooser
+        self.open_filechooser(instance, file_type='*.pkl', title='Load a Prediction Model')
+
+    def select_file(self, instance, selection, touch):
+        if selection:
+            self.filepath = selection[0]
+            if self.filepath.endswith('.xlsx'):
+                self.label.text = f'Selected Excel file: {os.path.basename(self.filepath)}'
+                self.predict_button.disabled = False
+            elif self.filepath.endswith('.pkl'):
+                self.label.text = f'Loaded model: {os.path.basename(self.filepath)}'
+                self.model = load(self.filepath)  # Load the new model
+            self.popup.dismiss()
+
     def predict(self, instance):
-        filepath = self.filechooser.selection[0]
-        data = pd.read_excel(filepath)
-        data = data[[coluna for coluna in data.columns if coluna not in ['nasogastric_reflux_ph', 'abdomo_protein', 'total_protein',
-       'abdomo_appearance']]]
+        data = pd.read_excel(self.filepath)
+        data = data[[col for col in data.columns if col not in ['nasogastric_reflux_ph', 'abdomo_protein', 'total_protein',
+                                                                'abdomo_appearance']]]
         predictions = self.model.predict(data)
-        self.label.text = 'Predictions: ' + ', '.join(map(str, predictions))
+        predictions_text = 'Predictions: ' + ', '.join(map(str, predictions))
+        
+        if len(predictions_text) > 100:  # Se o texto for muito longo para a label
+            # Use um TextInput dentro de um ScrollView
+            scroll_view = ScrollView(size_hint=(1, None), size=(Window.width, Window.height * 0.3))
+            text_input = TextInput(text=predictions_text, readonly=True, multiline=True)
+            scroll_view.add_widget(text_input)
+            
+            self.popup = Popup(title='Predictions', content=scroll_view,
+                               size_hint=(0.9, 0.5))
+            self.popup.open()
+        else:
+            self.label.text = predictions_text
+
 
 if __name__ == '__main__':
     PredictorApp().run()
